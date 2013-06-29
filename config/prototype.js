612d328b0825e2bf925c9521ff5a749c980e0ced
gaishimo <gaithimo@gmail.com>
2013-06-29 12:26:50 +0900

    add redis config to env:prototype

--- config/prototype.js
+++ config/prototype.js
@@ -4,6 +4,12 @@ module.exports= {
   db: {
     mongodb: {
       url : "mongodb://localhost:27017/sentences?auto_reconnect"
+    },
+    redis_session:{
+        host: "localhost",
+        port:3679,
+        database_index: 0,
+        password: ""
     }
   }
 };
