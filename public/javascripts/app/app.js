d3334bda8bb266a068c921443956cc9bc620604d
gaishimo <gaithimo@gmail.com>
2013-07-15 19:31:03 +0900

    remove unneeded files

--- public/javascripts/app/app.js
+++
@@ -1,9 +0,0 @@
-define(['./init', './views/main'], function(init, MainView){
-  var app = {
-    init: function () {
-      this.mainView = new MainView();
-      this.mainView.render();
-    }
-  };
-  return app;
-});
--- public/javascripts/app/app_attrs.js
+++
@@ -1,6 +0,0 @@
-define([], function(){
-  var user_id = $('body').attr('data-user-id');
-  return {
-    user_id: user_id
-  };
-});
